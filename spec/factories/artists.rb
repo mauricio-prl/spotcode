6b4fe4d8f7d09b41f1fada99325678fc356dcc75
Mauricio P <mauriciopdvg@gmail.com>
2020-04-18 11:32:47 -0300

    Add Artist model

---
+++ spec/factories/artists.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :artist do
+    name { FFaker::Name.name }
+  end
+end
---
+++ spec/models/artist_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Artist, type: :model do
+  describe 'validations' do
+    subject { build(:artist) }
+
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+  end
+end
