6709808b7236d4e894c22d6a64de1755933873d7
Mauricio Pereira <mauriciopdvg@gmail.com>
2020-04-18 10:05:21 -0300

    Add devise and generate User (#1)
    * Add devise and generate User
    
    * Install FFaker gem, and fix the user factory

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.5'
 
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'devise'
 gem 'jbuilder', '~> 2.7'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 4.1'
@@ -15,6 +16,7 @@ gem 'webpacker', '~> 4.0'
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'factory_bot_rails'
+  gem 'ffaker'
   gem 'fuubar'
   gem 'rspec-rails'
   gem 'rubocop'
--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -2,10 +2,23 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :request do
   describe 'GET /index' do
-    it 'returns http ok' do
-      get '/'
+    context 'when not logged' do
+      it 'returns http 302' do
+        get '/'
 
-      expect(response).to have_http_status(:ok)
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    context 'when logged in' do
+      let(:user) { create(:user) }
+
+      it 'returns http ok' do
+        sign_in(user)
+        get '/'
+
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 end
