fe5f33d6e502c02fdb109a5f6b50e6f99f99d034
Mauricio P <mauriciopdvg@gmail.com>
2020-04-19 10:57:14 -0300

    Add SearchController

---
+++ spec/requests/api/v1/search_request_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::SearchController, type: :request do
+  let(:user) { create(:user) }
+
+  before { sign_in(user) }
+
+  describe 'GET #index' do
+    subject { get '/api/v1/search' }
+
+    it 'has http status ok' do
+      subject
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'calls Song.where' do
+      allow(Song).to receive(:where)
+      allow(Album).to receive(:where)
+      allow(Artist).to receive(:where)
+
+      expect(Song).to receive(:where)
+      expect(Album).to receive(:where)
+      expect(Artist).to receive(:where)
+
+      subject
+    end
+  end
+end
