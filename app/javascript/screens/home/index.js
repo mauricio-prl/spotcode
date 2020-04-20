55d35042e711e8a1ee79f2a4e134573b69155640
Mauricio P <mauriciopdvg@gmail.com>
2020-04-19 22:30:28 -0300

    Add React Routes

--- app/javascript/screens/home/index.js
+++ app/javascript/screens/home/index.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from 'react';
-
+import React, { Fragment, useState } from 'react';
+      
 const HomeScreen = () => {
-  return (
+  return(
     <Fragment>
       <div>Home Screen</div>
     </Fragment>
-  )
+  );
 }
 
 export default HomeScreen;
