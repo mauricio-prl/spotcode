4637789d64fbad75394a7927d37d8a00584b30a9
Mauricio P <mauricio@codebit.com.br>
2020-04-25 13:41:39 -0300

    add Navbarfooter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery unless: -> { request.format.json? }
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     '/discovery'
   end
 end
